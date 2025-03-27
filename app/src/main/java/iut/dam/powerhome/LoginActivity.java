a3d94b0bb8a4f269f3fdf0ddc3114930faad14ea
sutakos <jedilucascaro@gmail.com>
2025-03-27 12:31:21 +0100

    LoginActivity finis

--- app/src/main/java/iut/dam/powerhome/LoginActivity.java
+++ app/src/main/java/iut/dam/powerhome/LoginActivity.java
@@ -1,15 +1,22 @@
 package iut.dam.powerhome;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+import com.koushikdutta.ion.Response;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -20,6 +27,10 @@ import iut.dam.powerhome.databinding.ActivityLoginBinding;
 
 public class LoginActivity extends AppCompatActivity {
 
+    ProgressDialog pDialog;
+    EditText mail;
+    EditText mdp;
+
     private AppBarConfiguration appBarConfiguration;
 
     @Override
@@ -28,19 +39,21 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
 
-        EditText mail = findViewById(R.id.email);
-        EditText mdp = findViewById(R.id.mdp);
+        mail = findViewById(R.id.email);
+        mdp = findViewById(R.id.mdp);
         Button loginbtn = findViewById(R.id.loginbtn);
 
         loginbtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                String email = mail.getText().toString();
+                String password = mdp.getText().toString();
 
-                Bundle bundle = new Bundle();
-                bundle.putString("mail", mail.getText().toString());
-                bundle.putString("mdp", mdp.getText().toString());
-                intent.putExtras(bundle);
-                startActivity(intent);
+                if(email.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(LoginActivity.this, "Veuillez remplir tous les champs", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                attemptLogin(email, password);
             }
         });
     }
@@ -50,5 +63,63 @@ public class LoginActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_login);
         return NavigationUI.navigateUp(navController, appBarConfiguration)
                 || super.onSupportNavigateUp();
+    }
+
+
+    private void attemptLogin(String email, String password) {
+        pDialog = new ProgressDialog(LoginActivity.this);
+        pDialog.setMessage("Connexion en cours...");
+        pDialog.setIndeterminate(true);
+        pDialog.setCancelable(false);
+        pDialog.show();
+
+        String urlString = "http://192.168.1.19/powerhome_server/login.php?email="
+                + email + "&password=" + password;
+        Ion.with(LoginActivity.this)
+                .load(urlString) // Méthode GET par défaut
+                .asJsonObject()
+                .withResponse()
+                .setCallback(new FutureCallback<Response<JsonObject>>() {
+                    @Override
+                    public void onCompleted(Exception e, Response<JsonObject> result) {
+                        Log.d("RESULT",result.toString());
+                        pDialog.dismiss();
+                        if(e != null) {
+                            Toast.makeText(LoginActivity.this, "Erreur de connexion", Toast.LENGTH_SHORT).show();
+                            Log.e("LOGIN_ERROR", e.getMessage());
+                            return;
+                        }
+
+                        if(result == null) {
+                            Toast.makeText(LoginActivity.this, "Aucune réponse du serveur", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        JsonObject response = result.getResult();
+                        if(response != null && response.has("token")) {
+                            // Connexion réussie
+                            String token = response.get("token").getAsString();
+                            String expiredAt = response.get("expired_at").getAsString();
+
+                            // Stocker le token et la date d'expiration pour les requêtes futures
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            intent.putExtra("token", token);
+                            intent.putExtra("expired_at", expiredAt);
+                            Bundle bundle = new Bundle();
+                            bundle.putString("mail", mail.getText().toString());
+                            bundle.putString("mdp", mdp.getText().toString());
+                            intent.putExtras(bundle);
+                            startActivity(intent);
+                            finish();
+                        } else {
+                            // Échec de la connexion
+                            String errorMessage = "Échec de la connexion";
+                            if(response != null && response.has("message")) {
+                                errorMessage = response.get("message").getAsString();
+                            }
+                            Toast.makeText(LoginActivity.this, errorMessage, Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
