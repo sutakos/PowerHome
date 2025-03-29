2309d5e33a7919eeb06dc4da5745c13e4d0eb906
RefonteSite <lucie.phung@etu.u-paris.fr>
2025-03-29 19:55:07 +0100

    Ajout vérification RegisterActivity

--- app/src/main/java/iut/dam/powerhome/RegisterActivity.java
+++ app/src/main/java/iut/dam/powerhome/RegisterActivity.java
@@ -1,5 +1,7 @@
 package iut.dam.powerhome;
 
+import android.content.Intent;
+import android.content.pm.LabeledIntent;
 import android.os.Bundle;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -8,7 +10,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -19,14 +24,11 @@ import iut.dam.powerhome.databinding.ActivityRegisterBinding;
 
 public class RegisterActivity extends AppCompatActivity {
 
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_register);
-
 
         Spinner sp = (Spinner) findViewById(R.id.phone_spinner);
         String[] phonePre = new String[]{"+33",
@@ -39,6 +41,25 @@ public class RegisterActivity extends AppCompatActivity {
         "+55"};
         ArrayAdapter<CharSequence> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,phonePre);
         sp.setAdapter(adapter);
+
+        EditText editTextEmail = findViewById(R.id.email_register);
+        String inputTextE = editTextEmail.getText().toString().trim();
+        EditText editTextPass = findViewById(R.id.password_register);
+        String inputTextP = editTextPass.getText().toString().trim();
+        Button btn_register = findViewById(R.id.button_register);
+
+        btn_register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(inputTextE.isEmpty() || inputTextP.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Veuillez saisir les champs", Toast.LENGTH_SHORT).show();
+                    return;
+                } else {
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
 
     }
 
